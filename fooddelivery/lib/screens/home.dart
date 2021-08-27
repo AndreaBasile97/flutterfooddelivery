1325c899de8ae919373f6cadd9ba682d7a9e6d63
Acer 729465 <vanni.colasanto97@gmail.com>
2021-08-27 12:45:35 +0200

    fix CustomTile

--- fooddelivery/lib/screens/home.dart
+++ fooddelivery/lib/screens/home.dart
@@ -116,6 +116,7 @@ class _CustomTile extends State<CustomTile> {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Padding(
@@ -124,25 +125,20 @@ class _CustomTile extends State<CustomTile> {
         ),
         Expanded(
           flex: 11,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.name,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(widget.descrizione),
-                      Text(widget.prezzo)
-                    ],
-                  ))
-            ],
-          ),
+          child: Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.name,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(widget.descrizione),
+                  Text(widget.prezzo)
+                ],
+              )),
         ),
         Expanded(
           flex: 5,
