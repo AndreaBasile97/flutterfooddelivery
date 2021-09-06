a0c3c51b739f8e5c64160cf279b5f462796fcd78
Acer 729465 <vanni.colasanto97@gmail.com>
2021-09-06 22:02:37 +0200

    firestore

--- fooddelivery/lib/interactive/carrello.dart
+++ fooddelivery/lib/interactive/carrello.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:fooddelivery/screens/home.dart';
 
 class Carrello extends ChangeNotifier {
@@ -22,11 +24,40 @@ class Carrello extends ChangeNotifier {
     print(printLista());
   }
 
-  String printLista() {
+  static String printLista() {
     String result = "lista:\n";
     lista.forEach((element) {
       result += element.title + " " + element.quantity.toString() + "\n";
     });
     return result;
+  }
+
+  static void doOrdine() {
+    FirebaseFirestore.instance
+        .collection('ordinazioni')
+        .add({'ordine': printLista()});
+  }
+
+  static Widget readOrdine() {
+    return Scaffold(
+      body: StreamBuilder(
+        stream:
+            FirebaseFirestore.instance.collection('ordinazioni').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            children: snapshot.data.docs.map((ordinazione) {
+              return Container(
+                child: Center(child: Text(ordinazione['ordine'])),
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
   }
 }
--- fooddelivery/lib/screens/cart.dart
+++ fooddelivery/lib/screens/cart.dart
@@ -24,19 +24,26 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // This is handled by the search bar itself.
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          ListView.builder(
-            itemBuilder: (BuildContext context, int index) =>
-                ProdottoItem(Carrello.lista[index]),
-            itemCount: Carrello.lista.length,
-          )
-        ],
-      ),
-    );
+        // This is handled by the search bar itself.
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            ListView.builder(
+              itemBuilder: (BuildContext context, int index) =>
+                  ProdottoItem(Carrello.lista[index]),
+              itemCount: Carrello.lista.length,
+            ),
+          ],
+        ),
+        bottomNavigationBar: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Carrello.doOrdine();
+              },
+              child: Text('Fixed Button'),
+            )));
   }
 }
 
