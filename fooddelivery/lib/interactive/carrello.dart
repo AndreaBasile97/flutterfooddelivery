49968c3a94c036837a3741634b24fbec28b68a94
Andrea Basile <andrea.basile@apuliasoft.com>
2021-09-07 03:07:13 +0200

    Fix read from db

--- fooddelivery/lib/interactive/carrello.dart
+++ fooddelivery/lib/interactive/carrello.dart
@@ -32,32 +32,19 @@ class Carrello extends ChangeNotifier {
     return result;
   }
 
-  static void doOrdine() {
+  static void ordina() {
     FirebaseFirestore.instance
         .collection('ordinazioni')
         .add({'ordine': printLista()});
   }
 
-  static Widget readOrdine() {
-    return Scaffold(
-      body: StreamBuilder(
-        stream:
-            FirebaseFirestore.instance.collection('ordinazioni').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return ListView(
-            children: snapshot.data.docs.map((ordinazione) {
-              return Container(
-                child: Center(child: Text(ordinazione['ordine'])),
-              );
-            }).toList(),
-          );
-        },
-      ),
-    );
+  static Future getAllOrdini(BuildContext context) async {
+    QuerySnapshot refOrdini =
+        await FirebaseFirestore.instance.collection("ordinazioni").get();
+    var ordini = [];
+    for (int i = 0; i < refOrdini.docs.length; i++) {
+      ordini.add(refOrdini.docs[i].data().toString());
+    }
+    print(ordini);
   }
 }
--- fooddelivery/lib/screens/cart.dart
+++ fooddelivery/lib/screens/cart.dart
@@ -1,49 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:fooddelivery/interactive/carrello.dart';
 import 'package:fooddelivery/screens/home.dart';
-import 'package:provider/provider.dart';
 
 class Cart extends StatelessWidget {
   const Cart({Key key}) : super(key: key);
 
-  static final widgets = <Widget>[CartScreen(), ShipmentScreen()];
+  //static final widgets = <Widget>[CartScreen(), ShipmentScreen()];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        fit: StackFit.expand,
-        children: [CartScreen()],
-      ),
-    );
+    TabController tabController;
+
+    return DefaultTabController(
+        length: 2,
+        // Use a Builder here, otherwise DefaultTabController.of(context) below
+        // returns null.
+        child: Builder(
+          builder: (BuildContext context) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: IconTheme(
+                    data: IconThemeData(
+                      size: 128.0,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    child: TabBarView(
+                        controller: tabController,
+                        children: [CartScreen(), ShipmentScreen()]),
+                  ),
+                ),
+                const TabPageSelector(),
+              ],
+            ),
+          ),
+        ));
   }
 }
 
+// ignore: must_be_immutable
 class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // This is handled by the search bar itself.
-        resizeToAvoidBottomInset: false,
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            ListView.builder(
-              itemBuilder: (BuildContext context, int index) =>
-                  ProdottoItem(Carrello.lista[index]),
-              itemCount: Carrello.lista.length,
-            ),
-          ],
-        ),
-        bottomNavigationBar: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Carrello.doOrdine();
-              },
-              child: Text('Fixed Button'),
-            )));
+      // This is handled by the search bar itself.
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          ListView.builder(
+            itemBuilder: (BuildContext context, int index) =>
+                ProdottoItem(Carrello.lista[index]),
+            itemCount: Carrello.lista.length,
+          ),
+        ],
+      ),
+      bottomNavigationBar: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Carrello.getAllOrdini(context);
+            },
+            child: Text('ordine'),
+          )),
+    );
   }
 }
 
